f53fd7542fc842741cb477d1414dff6a27804e32
ZenAyush <as1087e@gmail.com>
2023-04-04 19:37:52 +0530

    Random Module

---
+++ Random Module.py
@@ -0,0 +1,10 @@
+import random
+
+random_interger = random.randint(1,10)
+print(random_interger)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")
