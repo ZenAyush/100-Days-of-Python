a759bf4bf24d16e6f2d334c677f2fe7c7dcac877
ZenAyush <as1087e@gmail.com>
2023-04-08 15:01:24 +0530

    PyPassword

--- PyPassword Generator.py
+++ PyPassword Generator.py
@@ -1,38 +1,38 @@
-#Password Generator Project
+# This program generates a password using letters, numbers, and symbols.
 import random
+
+# Lists of possible letters, numbers, and symbols.
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# Ask user for the number of letters, numbers, and symbols to include in the password.
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+pass_list = []
 
+# Add random letters to the password.
+for char in range(1, nr_letters + 1):
+    pass_list += random.choice(letters)
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-password = []
-rand_letters = random.sample(letters,nr_letters)
-rand_numbers = random.sample(numbers,nr_numbers)
-rand_symbols = random.sample(symbols,nr_symbols)
+# Add random symbols to the password.
+for char in range(1, nr_symbols +1):
+    pass_list += random.choice(symbols)
 
-combo = [rand_letters,rand_numbers,rand_symbols]
+# Add random numbers to the password.
+for char in range(1, nr_numbers +1):
+    pass_list += random.choice(numbers)
 
-# for i in combo:
-#     for j in i:
-#         password = j
-# print(password)
-zigzag = []
-for i in combo:
-    zigzag = random.randint(0, 2)
-print(combo[zigzag])
-# for i in zigzag:
-#     i = int(i)
-#     print(combo[i])
+# Shuffle the password to randomize the order of characters.
+random.shuffle(pass_list)
 
-# print(p_letters)
-# print(password)
+# Combine the characters in the password list to form the password string.
+password = ""
+for char in pass_list:
+    password += char
+
+# Print the password.
+print(f"Your password is: {password}")
